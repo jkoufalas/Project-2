738a78d6076e89047759d357fd0d4e3b0241bcb8
Carol <carolinemaee@live.com>
2022-09-13 13:31:06 +0930

    buttons and script added to go to category threads

---
+++ public/js/threads.js
@@ -0,0 +1,11 @@
+const categorySpan = document.querySelector('.category-span');
+
+categorySpan.addEventListener('click', function(event) {
+    let element = event.target;
+    let id = element.getAttribute('id');
+
+    if (element.matches("button") === true) {
+        document.location.replace('/threads/' + id);
+        console.log(id);
+    }
+});
