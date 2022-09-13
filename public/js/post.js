15fa967b18313193dcce96cf369d74c5e120ff8e
jkoufalas <john@koufalas.com>
2022-09-13 21:02:29 +0930

    added posting to thread

--- public/js/post.js
+++ public/js/post.js
@@ -1,19 +1,24 @@
 const postFormHandler = async (event) => {
   event.preventDefault();
 
-  const post = document.querySelector("#post").value.trim();
+  const post = document.querySelector("#new-post").value.trim();
+  const thread_id = document.querySelector("#thread-id").getAttribute("thread");
+
+  console.log("```````````````````````````");
+  console.log(thread_id);
+  console.log("```````````````````````````");
 
   if (post) {
     const response = await fetch("/api/post", {
       method: "POST",
-      body: JSON.stringify({ post }),
+      body: JSON.stringify({ post, thread_id }),
       headers: { "Content-Type": "application/json" },
     });
 
     const answer = await response.json();
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/thread/" + thread_id);
     } else {
       alert(answer.errors[0].message);
     }
