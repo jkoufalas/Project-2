1523be55a79e9d4961ef437225c3d29b562466b6
jkoufalas <john@koufalas.com>
2022-09-19 19:20:27 +0930

    fixed reimplementation of SMTP for nodemailer after testing

--- config/email_connection.js
+++ config/email_connection.js
@@ -2,15 +2,21 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 let emailConnection = new nodemailer.createTransport({
-  let emailConnection = new nodemailer.createTransport({
-    host: process.env.EMAIL_HOST, //smpt address
-    port: process.env.EMAIL_PORT, // email port
-    secure: process.env.EMAIL_SECURE, // SSL
-    auth: {
-      user: process.env.EMAIL_USER, //"username",
-      pass: process.env.EMAIL_PASSWORD, //"password",
-    },
-  });
+  host: process.env.EMAIL_HOST, //smpt address
+  port: process.env.EMAIL_PORT, // email port
+  secure: process.env.EMAIL_SECURE, // SSL
+  auth: {
+    user: process.env.EMAIL_USER, //"username",
+    pass: process.env.EMAIL_PASSWORD, //"password",
+  },
+});
+
+emailConnection.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
 });
 
 module.exports = emailConnection;
