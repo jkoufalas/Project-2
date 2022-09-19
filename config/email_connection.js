6739376bf7bea4871cf91bc6ebd09d9ae45d9f98
jkoufalas <john@koufalas.com>
2022-09-19 18:26:41 +0930

    changed nodemail to remove user/password because of heroku for submission

--- config/email_connection.js
+++ config/email_connection.js
@@ -2,12 +2,15 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 let emailConnection = new nodemailer.createTransport({
-  service: "Outlook365", //"Outlook365" is preconfigured, but you can setup any other email client supported by nodemailer
-  auth: {
-    user: "group4project2@hotmail.com", //"username",
-    pass: "Group!4Project!2", //"password",
-  },
-  //would use dotenv but cannot use .env on heroku and therefore for demo it requires this
+  let emailConnection = new nodemailer.createTransport({
+    host: process.env.EMAIL_HOST, //smpt address
+    port: process.env.EMAIL_PORT, // email port
+    secure: process.env.EMAIL_SECURE, // SSL
+    auth: {
+      user: process.env.EMAIL_USER, //"username",
+      pass: process.env.EMAIL_PASSWORD, //"password",
+    },
+  });
 });
 
 module.exports = emailConnection;
